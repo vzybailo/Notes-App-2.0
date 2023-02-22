53df13aeaefa697195e0f00705d29e18fc449b86
MAXBIT\v.zybailo <v.zybailo@jetmail.cc>
2023-02-22 18:42:53 +0300

    add list comp

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,21 @@
           @store="addNote"
         />
         <h2>{{ titleNotes }}</h2>
-        <ul class="notes__list">
-            <li v-for="(note, index) in notes" :key="index" class="notes__item">
-                <div class="notes__header">
-                  <h4 class="notes__title">{{note.title}}</h4>
-                  <div @click="removeNote" class="notes__delete">x</div>
-                </div>
-                <div class="notes__desc">{{note.descr}}</div>
-                <div class="notes__date">{{note.date}}</div>
-            </li>
-        </ul>
+        <notesList 
+          @removeNote="removeNote"
+          :notes="notes"
+        />
   </div>
 </template>
 
 <script>
 import errorMessage from './components/ErrorMessage.vue'
 import newNote from './components/NewNote.vue'
+import notesList from './components/NotesList.vue'
 
 export default {
   components: {
-    errorMessage, newNote
+    errorMessage, newNote, notesList
   },
   data () {
     return {
---
+++ src/components/NotesList.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="notes__list">
+        <li v-for="(note, index) in notes" :key="index" class="notes__item">
+            <div class="notes__header">
+                <h4 class="notes__title">{{note.title}}</h4>
+                <div @click="removeNote" class="notes__delete">x</div>
+            </div>
+            <div class="notes__desc">{{note.descr}}</div>
+            <div class="notes__date">{{note.date}}</div>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+      notes: {
+        type: Array,
+        required: true
+      }
+    },
+    methods: {
+        removeNote() {
+            this.$emit('removeNote', this.notes)
+        }
+    }
+};
+</script>
+
+<style>
+  
+</style>
